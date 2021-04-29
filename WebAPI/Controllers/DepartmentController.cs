294709693d891062352735b600c3549073229851
tkmik <tkach_nikita@outlook.com>
2021-04-29 14:05:01 +0300

    created a new project to interact with ASP.NET Core API

---
+++ WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : Controller
+    {
+        //private readonly IConfiguration _configuration;
+        //public DepartmentController(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
+        private readonly AppDbContext _dbContext;
+        public DepartmentController(AppDbContext db)
+        {
+            _dbContext = db;
+        }
+        [HttpGet]
+        public async Task<JsonResult> Get()
+        {
+            //string query = @"SELECT DepartmentId, DepartmentName FROM dbo.Department";
+            //DataTable table = new DataTable();
+            //string sqlDataSource = _configuration.GetConnectionString("Default");
+            //SqlDataReader reader;
+            //using (SqlConnection connection = new SqlConnection(sqlDataSource))
+            //{
+            //    await connection.OpenAsync();
+            //    using (SqlCommand command = new SqlCommand(query, connection))
+            //    {
+            //        reader = command.ExecuteReader();
+            //        table.Load(reader);
+
+            //        await reader.CloseAsync();
+            //        await connection.CloseAsync();
+            //    }
+            //}
+            var table = await _dbContext.Department.Select(d => new { d.DepartmentId, d.DepartmentName }).ToListAsync();
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ WebAPI/Models/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            var connectionString = config.GetConnectionString("Default");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
